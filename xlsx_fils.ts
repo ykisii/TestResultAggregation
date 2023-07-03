f918674978cecfcd22df0f521ec2a653ed17b0b7
ykisii <ishiiyuuki@gmail.com>
2023-07-03 10:20:40 +0900

    add: Test Item for analyze

--- xlsx_fils.ts
+++ xlsx_fils.ts
@@ -1,8 +1,10 @@
 import * as XLSX from "https://cdn.sheetjs.com/xlsx-0.18.3/package/xlsx.mjs";
 import { TestSource } from "./test_source.ts";
+import { TestItem } from "./test_item.js";
 
 export class XlsxFile implements TestSource {
   protected wb: any;
+  protected items: Array<TestItem>;
 
   constructor() {
     this.wb = null;
@@ -11,6 +13,7 @@ export class XlsxFile implements TestSource {
   open(filepath: string): void {
     const u8: Uint8Array = Deno.readFileSync(filepath);
     this.wb = XLSX.read(u8);
+    this.items = new Array<TestItem>(); 
   }
   analyze(): void {
     if (!this.wb) throw new Error("file has not opend.");
