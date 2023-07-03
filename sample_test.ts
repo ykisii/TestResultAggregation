b8d06e735f85b50c5ca124a41d013658db7c763f
ykisii <ishiiyuuki@gmail.com>
2023-07-03 17:01:45 +0900

    mod: xlsx has many locations

--- sample_test.ts
+++ sample_test.ts
@@ -1,30 +1,34 @@
 import { CellLocation } from "./cell_location.ts";
+import { IntegrationTest } from "./integration_test.ts";
 import { XlsxFile } from "./mod.ts"
 
 export type LocationsFormat = {
+    sheetName: string,
     no: CellLocation,
     type: CellLocation,
     result: CellLocation
-} | undefined;
+};
 
 export class SampleTest extends XlsxFile {
   //private locations: CellLocation;
-  #locations: LocationsFormat;
+  #locations: Array<LocationsFormat>;
 
-  set locations(locations: LocationsFormat) {
+  set locations(locations: Array<LocationsFormat>) {
     this.#locations = structuredClone(locations);
   }
 
-  constructor(filepath: string) {
+  constructor(testName: string, filepath: string) {
     super();
     super.open(filepath);
-    this.#locations = undefined;
+    this.#locations = new Array<LocationsFormat>;
   }
 
   analyze(): void {
     super.analyze();
-    console.log(this.wb);
-    const v = this.wb.Sheets['Sheet1']['A3'].v;
-    console.log(v); 
+  }
+
+  getData() {
+    super.getData();
+
   }
 }
